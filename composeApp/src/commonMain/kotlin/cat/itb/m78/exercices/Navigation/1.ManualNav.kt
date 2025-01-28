a9af57e89d475383d76636441a8cb4bcaea7c73e
unai.arevalo.7e8 <unai.arevalo.7e8@itb.cat>
2025-01-28 10:19:06 +0100

    Update

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Navigation/1.ManualNav.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Navigation/1.ManualNav.kt
@@ -1,11 +1,8 @@
 package cat.itb.m78.exercices.Navigation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -14,7 +11,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 
