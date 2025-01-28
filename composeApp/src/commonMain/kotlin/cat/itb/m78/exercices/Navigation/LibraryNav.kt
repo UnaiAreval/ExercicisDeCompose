3cc10c7487472ccc9b7194fe5aed9a3020dd2bf0
unai.arevalo.7e8 <unai.arevalo.7e8@itb.cat>
2025-01-28 10:39:56 +0100

    Update

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Navigation/LibraryNav.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Navigation/LibraryNav.kt
@@ -1,2 +1,153 @@
 package cat.itb.m78.exercices.Navigation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import cat.itb.m78.exercices.State.GoodTime
+import cat.itb.m78.exercices.State.SayHello
+import cat.itb.m78.exercices.State.diceRoller
+import cat.itb.m78.exercices.State.secretNum
+import cat.itb.m78.exercices.Stateless.ContactApp
+import cat.itb.m78.exercices.Stateless.Resource
+import cat.itb.m78.exercices.Stateless.Welcome
+import cat.itb.m78.exercices.Stateless.helloWorld
+
+//Stateless Exercises Screens
+@Composable
+fun StatelessScreen(){
+    Column {
+        Button(onClick = {
+
+        }){
+            Text("HelloWorld")
+        }
+
+        Button(onClick = {
+
+        }){
+            Text("Welcome")
+        }
+
+        Button(onClick = {
+
+        }){
+            Text("Resource")
+        }
+
+        Button(onClick = {
+
+        }){
+            Text("Contact")
+        }
+
+        Button(onClick = {
+
+        }){
+            Text("MessagesList")
+        }
+    }
+}
+
+@Composable
+fun HellWorldScreen(){
+    helloWorld()
+}
+@Composable
+fun WelcomeScreen(){
+    Welcome()
+}
+@Composable
+fun ResourceScreen(){
+    Resource()
+}
+@Composable
+fun ContactScreen(){
+    ContactApp()
+}
+@Composable
+fun MessagesListScreen(){
+
+}
+
+//State Exercises Screens
+@Composable
+fun StateScreen(){
+    Column {
+        Button(onClick = {
+
+        }){
+            Text("GoodMorningAndNight")
+        }
+
+        Button(onClick = {
+
+        }){
+            Text("SayHelloScreen")
+        }
+
+        Button(onClick = {
+
+        }){
+            Text("SecretNumber")
+        }
+
+        Button(onClick = {
+
+        }){
+            Text("DiceRoller")
+        }
+    }
+}
+@Composable
+fun GoodMorningAndNight(){
+    GoodTime()
+}
+@Composable
+fun SayHelloScreen(){
+    SayHello()
+}
+@Composable
+fun SecretNumberScreen(){
+    secretNum()
+}
+@Composable
+fun DiceRollerScreen(){
+    diceRoller()
+}
+
+//View Model Exercises Screens
+@Composable
+fun ViewModelScreen(){
+    Column {
+        Button(onClick = {
+
+        }){
+            Text("Counter")
+        }
+
+        Button(onClick = {
+
+        }){
+            Text("ShoppingList")
+        }
+    }
+}
+
+//Navigation Exercises Screens
+@Composable
+fun NavigationScreen(){
+    Column {
+        Button(onClick = {
+
+        }){
+            Text("ManualNav")
+        }
+
+        Button(onClick = {
+
+        }){
+            Text("LibraryNav")
+        }
+    }
+}
