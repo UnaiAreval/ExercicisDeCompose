322e73fac5b995305bcebd2fa478fc9451d89318
unai.arevalo.7e8 <unai.arevalo.7e8@itb.cat>
2025-03-19 13:05:48 +0100

    19/3/2025 update

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/exercicis06_Api/3.EstatEmbassaments.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/exercicis06_Api/3.EstatEmbassaments.kt
@@ -1,11 +1,18 @@
 package cat.itb.m78.exercices.exercicis06_Api
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -20,7 +27,7 @@ import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.Month
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
@@ -29,7 +36,7 @@ import kotlinx.serialization.json.Json
 
 @Serializable
 data class Embassament(
-    @SerialName ("dia") val date: DateTimeUnit,
+    @SerialName ("dia") val date: String,
     @SerialName  ("estaci") val name: String,
     @SerialName ("nivell_absolut") val level: Double,
     @SerialName ("percentatge_volum_embassat") val volumePercent: Double,
@@ -61,7 +68,7 @@ object ScreenDestination {
     @Serializable
     data object ListEmbassaments
     @Serializable
-    data class InfoEmbassament(val embassament: Embassament)
+    data class InfoEmbassament(val embassamentName: String)
 }
 
 @Composable
@@ -77,7 +84,7 @@ fun EmbassamentScreen(){
             )
         }
         composable<ScreenDestination.InfoEmbassament> {backStack ->
-            val embassament = backStack.toRoute<ScreenDestination.InfoEmbassament>().embassament
+            val embassament = backStack.toRoute<ScreenDestination.InfoEmbassament>().embassamentName
             InfoEmbassament(
                 embassament,
                 navigateToListEmbassaments = { navController.navigate(ScreenDestination.ListEmbassaments) }
@@ -87,15 +94,27 @@ fun EmbassamentScreen(){
 }
 
 @Composable
-fun LlistaEmbassaments(list: List<Embassament>?, navigateToInfoEmbassament: (Embassament)-> Unit){
+fun LlistaEmbassaments(list: List<Embassament>?, navigateToInfoEmbassament: (String)-> Unit){
     if (list != null) {
-        for (i in 0..list.size) {
-            Row {
-                Text(list[i].name)
-                Button(onClick = {
-                    navigateToInfoEmbassament(list[i])
-                }) {
-                    Text("Veure les lectures")
+        for (i in 0..list.size - 2) {
+            for (j in 1..<list.size){
+                if (list[i].name == list[j].name){
+
+                }
+            }
+        }
+
+        LazyColumn (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            for (i in list.indices) {
+                item {
+                    Column (modifier = Modifier.padding(10.dp)) {
+                        Text(list[i].name)
+                        Button(onClick = {
+                            navigateToInfoEmbassament(list[i].name)
+                        }) {
+                            Text("Veure les lectures")
+                        }
+                    }
                 }
             }
         }
@@ -103,12 +122,23 @@ fun LlistaEmbassaments(list: List<Embassament>?, navigateToInfoEmbassament: (Emb
 }
 
 @Composable
-fun InfoEmbassament(embassament: Embassament, navigateToListEmbassaments: ()-> Unit){
+fun InfoEmbassament(embassamentName: String, navigateToListEmbassaments: ()-> Unit){
+    val list : List<Embassament>
     Column {
-        Text("${embassament.name} \n " +
-                "\n Data de la lectura: ${embassament.date} " +
-                "\nNivell absolut: ${embassament.level} " +
-                "\nVolum embassament: ${embassament.volume} Percentatge del volum de l'embassament: ${embassament.volumePercent}")
+        /*
+        LazyColumn (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            for (i in list.indices) {
+                item {
+                    Column (modifier = Modifier.padding(10.dp)) {
+                        Text("${list[i].name} \n " +
+                                "\n Data de la lectura: ${list[i].date} " +
+                                "\nNivell absolut: ${list[i].level} " +
+                                "\nVolum embassament: ${list[i].volume} Percentatge del volum de l'embassament: ${list[i].volumePercent}")
+                    }
+                }
+            }
+        }
+        */
         Button(onClick = {
             navigateToListEmbassaments()
         }){
@@ -116,3 +146,16 @@ fun InfoEmbassament(embassament: Embassament, navigateToListEmbassaments: ()-> U
         }
     }
 }
+/*
+object InfoEmbassamentApi{
+    val url = "https://analisi.transparenciacatalunya.cat/resource/gn9e-3qhr.json?estaci=$embassamentName"
+    val client = HttpClient(){
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+    suspend fun list() = client.get(url).body<List<Embassament>>()
+}
+*/
