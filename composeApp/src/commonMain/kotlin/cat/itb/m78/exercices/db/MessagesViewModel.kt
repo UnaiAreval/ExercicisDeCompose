5a5bb4476744dc3c1caab0239525d3b6dc9c75e9
unai.arevalo.7e8 <unai.arevalo.7e8@itb.cat>
2025-03-26 13:05:21 +0100

    26/3/2025 update

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/db/MessagesViewModel.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/db/MessagesViewModel.kt
@@ -1,7 +1,26 @@
 package cat.itb.m78.exercices.db
 
 import androidx.lifecycle.ViewModel
-
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+/*
 class MessagesViewModel : ViewModel(){
+    val messagesQueries = database.messagesQueries
+    val all = messagesQueries.selectAll().executeAsList()
 
+    fun Message(){
+        database.messagesQueries.insert("Holi, que tal?")
+    }
+
+    init {
+        viewModelScope.launch {
+            withContext(Dispatchers.Default){
+                database.messagesQueries.insert("some message")
+            }
+        }
+    }
 }
+
+ */
